5ab805fce34744f48181b7387f796b9a6c508617
renk <359876077@qq.com>
2019-01-13 23:04:47 +0800

    添加相交判断

--- canvas-painter.js
+++ canvas-painter.js
@@ -5,44 +5,114 @@ window.renk.canvasPainter = (function($) {
     var pub = {
         isActive: true,
         canvas: null,
-        mouse:null,//鼠标位置
+        startPoint:false,//鼠标起始位置
+        currentPoint:false,//鼠标起始位置
         origin:{x: 0, y: 0}, //格線原點座標
         scale:1,//缩放
         ctx: null,
+        creating:false,
+        targetObject:'Ray',
+        currentObject:false,
+        clickExtentPoint:35,
+        clickExtentLine:10,
         objects:{},
         init: function() {
-            this.canvas = document.getElementById("canvas1");
-            this.ctx = this.canvas.getContext('2d');
-            this.canvas.onmousemove = function(e){ onMouseMove(e)};
+            pub.canvas = document.getElementById("canvas1");
+            pub.ctx = this.canvas.getContext('2d');
+            pub.canvas.onmousemove = function(e){ onMouseMove(e)};
+            pub.canvas.onmousedown = function(e){ onMouseDown(e)};
+            pub.canvas.onmouseup = function(e){ onMouseUp(e)};
 
 
-            var point = {p1:{x:1,y:1},p2:{x:100,y:100}};
+            // var point = {p1:{x:1,y:1},p2:{x:100,y:100}};
             // this.ray({p1:{x:1,y:1},p2:{x:100,y:100}},2);
-            var blackLine = new BlackLine();
-            blackLine.draw(point,this.ctx);
+            // var blackLine = new BlackLine();
+            // blackLine.draw(point,this.ctx);
         },
-        ray: function(graph,color){
-          this.ctx.beginPath();
-          this.ctx.strokeStyle = color ? color : 'black';
-          ang1 = Math.atan2((graph.p2.x - graph.p1.x), (graph.p2.y - graph.p1.y)); //從斜率取得角度
-          cvsLimit = this.canvas.height + this.canvas.width;
-          this.ctx.moveTo(graph.p1.x, graph.p1.y);
-          this.ctx.lineTo(graph.p1.x + Math.sin(ang1) * cvsLimit, graph.p1.y + Math.cos(ang1) * cvsLimit);
-          this.ctx.stroke();
+        clear:function(){
+            pub.ctx.setTransform(1,0,0,1,0,0);
+            pub.ctx.clearRect(0, 0, pub.canvas.width, pub.canvas.height);
+            pub.ctx.setTransform(pub.scale,0,0,pub.scale,pub.origin.x, pub.origin.y);
         },
     };
 
+    //获取鼠标的位置
+    function getMousePoint(et) {
+        var mouse_nogrid = graphs.point((et.pageX - et.target.offsetLeft - pub.origin.x) / pub.scale, (et.pageY - et.target.offsetTop - pub.origin.y) / pub.scale); //滑鼠實際位置
+        return mouse_nogrid;
+    }
+
+    function onMouseDown(e) {
+        var currentPoint = getMousePoint(e);
+        if(pub.creating == false){
+            var selectedObject = getSelectedObject(currentPoint);
+            if(selectedObject){console.log('select one!!');
+                pub.startPoint = currentPoint;
+                selectedObject.selected = true;
+                pub.currentObject = selectedObject;
+                selectedObject.mouseDown(e,pub);
+            }else{
+                pub.startPoint = currentPoint;
+                if(pub.targetObject){
+                    pub.creating = true;
+                    var object = new window[pub.targetObject]();
+                    object.startPoint = pub.startPoint;
+                    object.creating = true;
+                    object.mouseDown(e);
+                    pub.currentObject = object;
+                    pub.objects[object.getId()] = object;
+                }
+            }
+
+        }else{
+            resetPub();
+        }
+    }
+
     function onMouseMove(e) {
         var et = e;
-        if (e.changedTouches) {console.log(111);
-            var et = e.changedTouches[0];
+        pub.currentPoint = getMousePoint(e);
+        if(pub.currentObject){
+            redraw();
+        }
+    }
+
+    function onMouseUp(event) {
+        //console.log('mouse up');
+        //console.log(pub.currentObject);
+        if(pub.currentObject){//console.log(pub.currentObject);
+            pub.currentObject.mouseUp(event);
         }
 
-        pub.mouse = graphs.point((et.pageX - e.target.offsetLeft - pub.origin.x) / pub.scale, (et.pageY - e.target.offsetTop - pub.origin.y) / pub.scale); //滑鼠實際位置
-        console.log(pub.mouse);
-        // console.log(e.target.offsetLeft);
-        // console.log(origin.x);
+        if(pub.creating == false){
+            resetPub();
+        }
     }
+
+    function resetPub() {
+        pub.creating = false;
+        pub.currentObject = false;
+        pub.startPoint = false;
+    }
+
+    function redraw() {
+        pub.clear();
+        for (var id in pub.objects){
+            // console.log(object);
+            pub.objects[id].update(pub).draw();
+        }
+    }
+
+    function getSelectedObject(currentPoint) {
+        for(var id in pub.objects){
+            if(pub.objects[id].setPub(pub).isSelected(currentPoint)){
+                return pub.objects[id];
+            }
+        }
+        return false;
+    }
+
+
 
     return pub;
 })(window.jQuery);
--- shape.js
+++ shape.js
@@ -210,6 +210,27 @@ var graphs = {
 };
 
 
+var helper = {
+    mouseOnPoint:function(mouse, point, clickExtentPoint){
+        return graphs.length_squared(mouse, point) < clickExtentPoint * clickExtentPoint;
+    },
+    mouseOnFirstPoint:function(mouse, point, clickExtentPoint){
+        helper.mouseOnPoint(mouse, point, clickExtentPoint);
+    },
+    convertObjectToLine:function(object){
+        return graphs.line({x:object.startPoint.x,y:object.startPoint.y}, {x:object.endPoint.x,y:object.endPoint.y});
+    },
+    rayIntersection: function(obj, ray) {
+        var rp_temp = graphs.intersection_2line(graphs.line(ray.p1, ray.p2), graphs.line(obj.p1, obj.p2));   //求光(的延長線)與物件(的延長線)的交點
+
+        if (graphs.intersection_is_on_segment(rp_temp, obj) && graphs.intersection_is_on_ray(rp_temp, ray))
+        {
+            //↑若rp_temp在ray上且rp_temp在obj上(即ray真的有射到obj,不是ray的延長線射到或射到obj的延長線上)
+            return rp_temp; //回傳光線的頭與鏡子的交點
+        }
+        return false;
+    }
+};
 
 
 window.objectTypes = {};
@@ -233,37 +254,188 @@ function getObject(objectName) {
 }
 
 function ObjectType(){
-  this.label = '';
+
 };
+
+ObjectType.prototype.startPoint = {};
+ObjectType.prototype.endPoint = {};
+ObjectType.prototype.points = {};
+ObjectType.prototype.pub = {};
+ObjectType.prototype.selected = false;//物体被选中
+ObjectType.prototype.selectedStartPoint = false;//起始点被选中
+ObjectType.prototype.selectedEndPoint = false;//结束点被选中
+ObjectType.prototype.creating = false;//正在被创建中
+ObjectType.prototype.effectRay = false;//物体是否会影响光
 
 ObjectType.prototype.shoot = function(){
   console.log('shoot！');
 };
 
-ObjectType.prototype.mouseDown = function(obj, mouse){
-  console.log('shoot！');
+ObjectType.prototype.mouseDown = function(event,pub){
+    this.pub = pub;
+    if(this.creating){
+        this.creating = false;
+    }
+     this.selectedStartPoint = this.selected && this.isSelectStartPoint();
+     this.selectedEndPoint = this.selected && this.isSelectEndPoint();
+
+};
+
+ObjectType.prototype.mouseMove = function(event){
+    this.draw(event);
+};
+ObjectType.prototype.mouseUp = function(event){
+    this.selected = false;
+    this.selectedStartPoint = false;
+    this.selectedEndPoint = false;
+};
+
+ObjectType.prototype.draw = function(){
+    console.log('draw！');
+};
+
+ObjectType.prototype.setPub = function(pub){
+    this.pub = pub;
+    return this;
+};
+
+ObjectType.prototype.update = function(pub){
+    this.pub = pub;
+    if(this.selected){
+        return this;
+    }
+    if(pub.currentObject.getId() == this.getId()){
+        this.startPoint = pub.startPoint;
+        this.endPoint = pub.currentPoint;
+    }
+
+    return this;
+};
+
+ObjectType.prototype.getId = function(){
+    if(!this.id){
+        this.id = this.constructor.name+new Date().getTime();
+    }
+    return this.id;
+};
+
+//检测自己是否被选中
+ObjectType.prototype.isSelected = function(mouse){
+    var segment={p1:this.startPoint,p2:this.endPoint};
+    var d_per = Math.pow((mouse.x - segment.p1.x) * (segment.p1.y - segment.p2.y) + (mouse.y - segment.p1.y) * (segment.p2.x - segment.p1.x), 2) / ((segment.p1.y - segment.p2.y) * (segment.p1.y - segment.p2.y) + (segment.p2.x - segment.p1.x) * (segment.p2.x - segment.p1.x)); //類似於滑鼠與直線垂直距離
+    var d_par = (segment.p2.x - segment.p1.x) * (mouse.x - segment.p1.x) + (segment.p2.y - segment.p1.y) * (mouse.y - segment.p1.y); //類似於滑鼠在直線上投影位置
+    return d_per < this.pub.clickExtentLine * this.pub.clickExtentLine && d_par >= 0 && d_par <= graphs.length_segment_squared(segment);
+};
+
+//检测自己起始点是否被选中
+ObjectType.prototype.isSelectStartPoint = function(){
+    var pub = this.pub;
+    if (helper.mouseOnPoint(pub.startPoint, this.startPoint,pub.clickExtentPoint) && graphs.length_squared(pub.startPoint, this.startPoint) <= graphs.length_squared(pub.startPoint, this.endPoint)) {
+        console.log('select first point');
+        return true;
+    }
+    return false;
+};
+
+//检测自己结束点是否被选中
+ObjectType.prototype.isSelectEndPoint = function(){
+    var pub = this.pub;
+    if (helper.mouseOnPoint(pub.startPoint, this.endPoint,pub.clickExtentPoint)){console.log('select end point');
+        return true;
+    }
+    return false;
 };
 
 
-//-----------
+//-----不透光墙------
 function BlackLine() {
-  this.name = 'Black Line';
+  this.title = '遮挡墙';
+  this.effectRay = 'block';
 };
 getPrototype(BlackLine,ObjectType);
-BlackLine.prototype.draw = function(obj, ctx) {
-  //var ctx = canvas.getContext('2d');
-  ctx.strokeStyle = 'rgb(70,35,10)';
-  ctx.lineWidth = 3;
-  ctx.lineCap = 'butt';
-  ctx.beginPath();
-  ctx.moveTo(obj.p1.x, obj.p1.y);
-  ctx.lineTo(obj.p2.x, obj.p2.y);
-  ctx.stroke();
-  ctx.lineWidth = 1;
+BlackLine.prototype.draw = function() {
+    if(this.selected){
+        //拖拽起始点
+        if(this.selectedStartPoint){
+            this.startPoint = this.pub.currentPoint;
+        }
+        //拖拽终点
+        if(this.selectedEndPoint){
+            this.endPoint = this.pub.currentPoint;
+        }
+    }
+    var ctx = this.pub.ctx;
+    ctx.strokeStyle = 'rgb(70,35,10)';
+    ctx.lineWidth = 3;
+    ctx.lineCap = 'butt';
+    ctx.beginPath();
+    ctx.moveTo(this.startPoint.x, this.startPoint.y);
+    ctx.lineTo(this.endPoint.x, this.endPoint.y);
+    ctx.stroke();
+    ctx.lineWidth = 1;
 };
 
 BlackLine.prototype.shoot = function(obj,ray){
   ray.exist = false;
 };
 
-//-----------
+BlackLine.prototype.isSelected = function(mouse){
+    var segment={p1:this.startPoint,p2:this.endPoint};
+    var d_per = Math.pow((mouse.x - segment.p1.x) * (segment.p1.y - segment.p2.y) + (mouse.y - segment.p1.y) * (segment.p2.x - segment.p1.x), 2) / ((segment.p1.y - segment.p2.y) * (segment.p1.y - segment.p2.y) + (segment.p2.x - segment.p1.x) * (segment.p2.x - segment.p1.x)); //類似於滑鼠與直線垂直距離
+    var d_par = (segment.p2.x - segment.p1.x) * (mouse.x - segment.p1.x) + (segment.p2.y - segment.p1.y) * (mouse.y - segment.p1.y); //類似於滑鼠在直線上投影位置
+    return d_per < this.pub.clickExtentLine * this.pub.clickExtentLine && d_par >= 0 && d_par <= graphs.length_segment_squared(segment);
+};
+
+//-----单条光线------
+function Ray() {
+    this.title = '光线';
+    this.finalPoint = {x:0,y:0};
+}
+getPrototype(Ray,ObjectType);
+Ray.prototype.draw = function() {
+    var pub = this.pub;
+    if(this.selected){
+        //拖拽起始点
+        if(this.selectedStartPoint){
+            this.startPoint = this.pub.currentPoint;
+        }
+        //拖拽终点
+        if(this.selectedEndPoint){
+            this.endPoint = this.pub.currentPoint;
+        }
+    }
+
+    var ang1 = Math.atan2((this.endPoint.x - this.startPoint.x), (this.endPoint.y - this.startPoint.y)); //從斜率取得角度
+    cvsLimit = pub.canvas.height + pub.canvas.width;
+    this.finalPoint.x = this.startPoint.x + Math.sin(ang1) * cvsLimit;
+    this.finalPoint.y = this.startPoint.y + Math.cos(ang1) * cvsLimit;
+
+    for (var id in pub.objects){
+        var object = pub.objects[id];
+        if(!object.effectRay) continue;
+        l1 = helper.convertObjectToLine(object);
+        l2 = helper.convertObjectToLine(this);
+        var intersection = helper.rayIntersection(l1,l2);
+        console.log(intersection);
+    }
+
+    var ctx = pub.ctx;
+    ctx.beginPath();
+    ctx.strokeStyle = 'red';
+    ctx.moveTo(this.startPoint.x, this.startPoint.y);
+    ctx.lineTo(this.finalPoint.x, this.finalPoint.y);
+    ctx.stroke();
+};
+
+//-------拖动点----------
+function Pointer() {
+    this.title = '拖动点';
+}
+getPrototype(Pointer,ObjectType);
+Pointer.prototype.draw = function() {
+    var ctx = this.pub.ctx;
+    ctx.beginPath();
+    ctx.fillStyle = 'red';
+    ctx.fillRect(this.startPoint.x - 2, this.startPoint.y - 2, 5, 5); //繪製填滿的矩形
+    ctx.stroke();
+};
